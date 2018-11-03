7801118d2daec8e5d3ebf167b0e747899643c6d1
Ezzeri Esa <ezzeri.esa@gmail.com>
2018-11-03 13:00:28 -0700

    refactored mapreduce

--- batch.py
+++ batch.py
@@ -3,36 +3,27 @@ from collections import defaultdict
 from functools import reduce
 
 
-def run_1():
-    data = pd.read_csv('train.csv')
-
-    feature_name = 'Pclass'
-    feature_uniques = set()
-    feature_array = data[feature_name].values.tolist()
-
-    for i, item in enumerate(feature_array):
-        if item not in feature_uniques:
-            feature_uniques.add(item)
-            result = map(lambda x: int(x == item), feature_array)
-            result = reduce(lambda x, y: x + y, result)
-            print item, result
-
-
-def run_2():
+def run():
     data = pd.read_csv('train.csv')
 
     feature_names = ['Pclass', 'Survived']
-    feature_uniques = set()
-    feature_array = data[feature_names].values.tolist()
+    feature_values = [None, [0, 1]]
 
-    for i, item in enumerate(feature_array):
-        if item[0] not in feature_uniques:
-            feature_uniques.add(item[0])
-            result = map(lambda x: int(x[0] == item[0] and x[1] == 1), feature_array)
+    feature_array = data[feature_names].values.tolist()
+    feature_counter = {}
+
+    for i, item in enumerate(feature_values):
+        if not item:
+            feature_values[i] = set([_[i] for _ in feature_array])
+
+    for item0 in feature_values[0]:
+        for item1 in feature_values[1]:
+            result = map(lambda x: int(x[0] == item0 and x[1] == item1), feature_array)
             result = reduce(lambda x, y: x + y, result)
-            print item[0], result
+            feature_counter[(item0, item1)] = result
+
+    print feature_counter
 
 
 if __name__ == '__main__':
-    run_1()
-    run_2()
+    run()
